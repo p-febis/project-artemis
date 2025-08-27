28e494b1496f7f230a0c88e3d30532c104a22dac
p-febis <preston.febis@yahoo.com>
2025-08-27 19:05:21 -0300

    Clear out repository

--- librebox/bootstrap/instances/InstanceTypes.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "bootstrap/instances/Part.h"
-#include "bootstrap/instances/CameraGame.h"
-#include "bootstrap/instances/Workspace.h"
-#include "bootstrap/instances/Script.h"
-#include "bootstrap/instances/LocalScript.h"
-// #include "bootstrap/instances/ModuleScript.h"
